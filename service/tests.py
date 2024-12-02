281dc11f4757429052b0f299049c697159cc1ea3
Amit Kumar <aksingh1109@gmail.com>
2024-12-02 16:13:33 +0530

    .

--- service/tests.py
+++ service/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from django.urls import reverse
 from .models import Vehicle, ServiceIssue
 from datetime import datetime, timedelta
